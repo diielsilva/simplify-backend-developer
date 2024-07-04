7c1a99146cccd7eed50d75b358960b3dd0f2b655
Daniel Silva <diielsilvasousa@gmail.com>
2024-07-04 13:46:06 -0300

    chore: creating todo model

---
+++ src/main/java/com/dev/backend_simplify/domain/enums/Priority.java
@@ -0,0 +1,2 @@
+package com.dev.backend_simplify.domain.enums;public enum Priority {
+}
---
+++ src/main/java/com/dev/backend_simplify/domain/models/Todo.java
@@ -0,0 +1,75 @@
+package com.dev.backend_simplify.domain.models;
+
+import com.dev.backend_simplify.domain.enums.Priority;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean completed;
+
+    @Enumerated(value = EnumType.STRING)
+    private Priority priority;
+
+    public Todo() {
+    }
+
+    public Todo(String name, String description, Boolean completed, Priority priority) {
+        this.name = name;
+        this.description = description;
+        this.completed = completed;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+}
