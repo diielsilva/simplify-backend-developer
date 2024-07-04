46d9a21ecae710ab7e351c4c91631ba9352731ea
Daniel Silva <diielsilvasousa@gmail.com>
2024-07-04 16:37:20 -0300

    chore: creating the default exception handler

--- src/main/java/com/dev/backend_simplify/api/handlers/ApiExceptionHandler.java
+++ src/main/java/com/dev/backend_simplify/api/handlers/ApiExceptionHandler.java
@@ -1,2 +1,46 @@
-package com.dev.backend_simplify.api.handlers;public class ApiExceptionHandler {
+package com.dev.backend_simplify.api.handlers;
+
+import com.dev.backend_simplify.common.dtos.ErrorResponse;
+import com.dev.backend_simplify.domain.exceptions.TodoNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = TodoNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTodoNotFound(TodoNotFoundException exception) {
+        return send(
+                HttpStatus.NOT_FOUND,
+                exception.getMessage()
+        );
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        return send(
+                HttpStatus.BAD_REQUEST,
+                "Missing required fields or they're invalid!"
+        );
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntime(RuntimeException exception) {
+        return send(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Something went wrong, if it persists contact the support!"
+        );
+    }
+
+    private ResponseEntity<ErrorResponse> send(HttpStatus status, String message) {
+        ErrorResponse response = new ErrorResponse(message);
+
+        return ResponseEntity
+                .status(status)
+                .body(response);
+    }
+
 }
