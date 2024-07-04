76276c9ceb000bc7e40991b35be3d4c947abeadd
Daniel Silva <diielsilvasousa@gmail.com>
2024-07-04 16:02:13 -0300

    chore: creating model mapper

--- src/main/java/com/dev/backend_simplify/common/mappers/ModelMapper.java
+++ src/main/java/com/dev/backend_simplify/common/mappers/ModelMapper.java
@@ -1,2 +1,31 @@
-package com.dev.backend_simplify.common.mappers;public class ModelMapper {
+package com.dev.backend_simplify.common.mappers;
+
+import com.dev.backend_simplify.common.dtos.TodoRequest;
+import com.dev.backend_simplify.common.dtos.TodoResponse;
+import com.dev.backend_simplify.domain.enums.Priority;
+import com.dev.backend_simplify.domain.models.Todo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModelMapper {
+
+    public Todo toModel(TodoRequest request) {
+        return new Todo(
+                request.name(),
+                request.description(),
+                request.completed(),
+                Priority.valueOf(request.priority())
+        );
+    }
+
+    public TodoResponse toResponse(Todo model) {
+        return new TodoResponse(
+                model.getId(),
+                model.getName(),
+                model.getDescription(),
+                model.getCompleted(),
+                model.getPriority()
+        );
+    }
+
 }
