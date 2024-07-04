999cab199581f31277e0665edac219a616c2a43e
Daniel Silva <diielsilvasousa@gmail.com>
2024-07-04 13:29:41 -0300

    initial commit

---
+++ src/main/java/com/dev/backend_simplify/BackendsimplifyApplication.java
@@ -0,0 +1,13 @@
+package com.dev.backend_simplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsimplifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/backend_simplify/BackendsimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.backend_simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
