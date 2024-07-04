a8127bf8cb7527d531ad02a7aef8d005d5f0879a
Daniel Silva <diielsilvasousa@gmail.com>
2024-07-04 20:50:28 -0300

    chore: adding tests to functionality that list all todos

--- src/test/java/com/dev/backend_simplify/api/controllers/TodoControllerTest.java
+++ src/test/java/com/dev/backend_simplify/api/controllers/TodoControllerTest.java
@@ -4,6 +4,7 @@ import com.dev.backend_simplify.common.dtos.ErrorResponse;
 import com.dev.backend_simplify.common.dtos.TodoRequest;
 import com.dev.backend_simplify.common.dtos.TodoResponse;
 import com.dev.backend_simplify.domain.enums.Priority;
+import com.dev.backend_simplify.domain.models.Todo;
 import com.dev.backend_simplify.domain.repositories.TodoRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,11 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.MySQLContainer;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -94,6 +98,108 @@ class TodoControllerTest {
             assertEquals("Missing required fields or they're invalid!", body.message());
         });
 
+    }
+
+    @Test
+    void shouldGetHighPriorityTodosOrderedByName() {
+        repository.saveAll(
+                List.of(
+                        new Todo(
+                                "Study English",
+                                "I should study more english",
+                                false,
+                                Priority.HIGH
+                        ),
+                        new Todo(
+                                "Learn Angular 18",
+                                "I should learn Angular 18",
+                                false,
+                                Priority.HIGH
+                        ),
+                        new Todo(
+                                "Read More",
+                                "I should read more",
+                                false,
+                                Priority.MEDIUM
+                        )
+                )
+        );
+
+        ResponseEntity<List<TodoResponse>> response = webClient.exchange(
+                "/api/v1/todos?priority={priority}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                },
+                "HIGH"
+        );
+
+        assertAll(() -> {
+            List<TodoResponse> body = response.getBody();
+
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertNotNull(body);
+            assertEquals(2, body.size());
+            assertEquals("Learn Angular 18", body.getFirst().name());
+            assertEquals("Study English", body.getLast().name());
+        });
+    }
+
+    @Test
+    void shouldNotGetTodos_WhenDoNotHaveTodosWithSelectedPriority() {
+        repository.saveAll(
+                List.of(
+                        new Todo(
+                                "Study English",
+                                "I should study more english",
+                                false,
+                                Priority.HIGH
+                        ),
+                        new Todo(
+                                "Learn Angular 18",
+                                "I should learn Angular 18",
+                                false,
+                                Priority.HIGH
+                        ),
+                        new Todo(
+                                "Read More",
+                                "I should read more",
+                                false,
+                                Priority.MEDIUM
+                        )
+                )
+        );
+
+        ResponseEntity<List<TodoResponse>> response = webClient.exchange(
+                "/api/v1/todos?priority={priority}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                },
+                "LOW"
+        );
+
+        assertAll(() -> {
+            List<TodoResponse> body = response.getBody();
+
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertNotNull(body);
+            assertTrue(body.isEmpty());
+        });
+    }
+
+    @Test
+    void shouldNotGetTodos_WhenPriorityIsInvalid() {
+        ResponseEntity<ErrorResponse> response = webClient.exchange(
+                "/api/v1/todos?priority={priority}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                },
+                "ULTRALOW"
+        );
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
 }
